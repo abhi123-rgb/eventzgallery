8d7d6eccc15c0736807790d9ad7169e75a38625a
Abhishek N M <141545566+abhi123-rgb@users.noreply.github.com>
2026-01-25 11:19:26 +0530

    initial up

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+  return (
+    <>
+      <div className='flex justify-center items-center h-screen bg-linear-to-r from-indigo-800 to-pink-400'>
+        <p className="text-4xl font-bold">EventsGallery.in</p>
+      </div>
+
+    </>
+  )
+}
+
+export default App
