0c0626eea9506288cc016e4ea8060998f2d249d1
Abhishek N M <141545566+abhi123-rgb@users.noreply.github.com>
2026-01-26 15:11:20 +0530

    Remove defaultTheme import and custom font configuration from vite.config.js.

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,8 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-const defaultTheme = require("tailwindcss/defaultTheme");
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  theme: {
-    extend: {
-      fontFamily: {
-        Poppins: ["Poppins", ...defaultTheme.fontFamily.sans],
-      },
-    },
-  },
 });
